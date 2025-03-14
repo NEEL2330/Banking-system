4ac2f9b90f6c7203ba9ca3ed2bb140a8276e19d9
Neel <neelbera1107@gmail.com>
2025-03-15 01:22:48 +0530

    Modified the code

--- API/handler/transaction.py
+++ API/handler/transaction.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
 import sys
 import os
-
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from db.transaction import trasnsaction, read, read_by_account_no, read_by_transaction_id, add_balance, subtract_balance
 
 app = Flask(__name__)
 @app.route('/transaction', methods = ['PUT','GET'])
+
 def transaction_route():
     if request.method=='PUT':
         body=request.json
@@ -15,6 +15,7 @@ def transaction_route():
         amount=body["amount"]
         trasnsaction(from_account_no, to_account_no, amount)
         return "Transaction successfully done"
+   
     else:
         transaction_id=request.args.get("transaction_id")
         account_no=request.args.get("account_no")
