54176c8b5e363dc8ac9eed91602e7ecbe819b5b9
Neel <neelbera1107@gmail.com>
2025-03-27 21:47:59 +0530

    DB modified

--- DB/customer.sql
+++ DB/customer.sql
@@ -1,8 +1,8 @@
 CREATE TABLE CustomerTable(
-        Customer_id INT AUTO_INCREMENT PRIMARY KEY,
-        Name VARCHAR(100),
-        City_name varchar(50),
-        Mobile_no bigint,
-        Email VARCHAR(255),
-        DOB INT
-    )
+    Customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100),
+    City_name varchar(50),
+    Mobile_no bigint,
+    Email VARCHAR(255),
+    DOB INT
+);
